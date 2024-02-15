cf354a891cc8fb34e80871eab1e83386a176017f
Ajita.Ganguly <ajita.ganguly@cognizant.com>
2024-02-15 21:14:36 +0530

    changes made for deployment

--- frontend/nasaappfrontend/src/App.js
+++ frontend/nasaappfrontend/src/App.js
@@ -1,18 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import NavBar from './components/NavBar';
 import Footer from "./components/Footer";
 import AuthRoutes from "./routes/AuthRoutes";
 import UnAuthRoutes from "./routes/UnAuthRoutes";
 import Details from "./pages/Details";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const loggedIn = useSelector((state) => state.loggedIn);
   return (
     <Router>
       <NavBar />
       <Routes>
-        {AuthRoutes}
-        {UnAuthRoutes}
+        {loggedIn ? (
+          <React.Fragment>
+            <Route element={<Navigate to="/" />} />
+            {AuthRoutes}
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <Route element={<Navigate to="/login" />} />
+            {UnAuthRoutes}
+          </React.Fragment>
+        )}
         <Route path="/details/:date" element={<Details />} />
 
       </Routes>
--- frontend/nasaappfrontend/src/routes/AuthRoutes.js
+++ frontend/nasaappfrontend/src/routes/AuthRoutes.js
@@ -4,7 +4,11 @@ import Wishlist from "../pages/Wishlist";
 import Apod from "../pages/Apod";
 import AuthGuard from "../guards/AuthGuards";
 
+
+const login = JSON.parse(localStorage.getItem("successResponse"))?.token || "";
+
 const AuthRoutes = [
+
 
     <Route key="Wishlist" path="/wishlist" element={<AuthGuard component={<Wishlist />} />} />,
     <Route key="Apod" path="/apod" element={<AuthGuard component={<Apod />} />} />
